cedd7d48779c1c5f00bf70a9f92927c299f5aec4
SMXTREME <132286622+SMXTREME@users.noreply.github.com>
2025-06-29 17:30:15 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const { signupRouter } = require('./signup');
 const { signinRouter } = require('./signin');
 const { dashboardRouter } = require('./dashboard');
 const { isLogin } = require('../middleware/isLogin');
+const RecurringExpense = require('../models/RecurringExpense');
+const redis = require('../redis');
+const OneTimeExpense = require('../models/OneTimeExpense');
 
 const indexRouter = Router();
 
@@ -24,6 +27,50 @@ indexRouter.use('/logout', async (req, res) => {
         res.clearCookie('rememberMe');
         res.redirect('/');
     });
+});
+
+indexRouter.get('/update', async (req, res) => {
+    const recurringExpenses = await RecurringExpense.find();
+
+    for (const recurringExpense of recurringExpenses) {
+        const today = new Date();
+        const nextExpenseDate = new Date(recurringExpense.nextExpenceAt);
+
+        const isToday =
+            today.getFullYear() === nextExpenseDate.getFullYear() &&
+            today.getMonth() === nextExpenseDate.getMonth() &&
+            today.getDate() === nextExpenseDate.getDate();
+
+        if (isToday) {
+            const oneTimeExpense = new OneTimeExpense({
+                amount: recurringExpense.amount,
+                dateAndTime: recurringExpense.nextExpenceAt,
+                description: recurringExpense.description,
+                title: recurringExpense.title,
+                userid: recurringExpense.userid,
+            });
+            await oneTimeExpense.save();
+            await redis.set(
+                `${oneTimeExpense._id}-ote`,
+                JSON.stringify(oneTimeExpense),
+                'EX',
+                1800
+            );
+            recurringExpense.oneTimePayments.push(`${oneTimeExpense.id}`);
+        }
+
+        recurringExpense.lastExpenceAddedAt = recurringExpense.nextExpenceAt;
+        recurringExpense.nextExpenceAt = new Date(
+            getNextExpenseDate(
+                recurringExpense.nextExpenceAt,
+                recurringExpense.wayOfRepeats.repeats,
+                recurringExpense.wayOfRepeats.every
+            )
+        );
+
+        await recurringExpense.save();
+        await redis.set(`${recurringExpense._id}-re`, JSON.stringify(recurringExpense), 'EX', 1800);
+    }
 });
 
 module.exports = { indexRouter };
--- routes/recurringExpense.js
+++ routes/recurringExpense.js
@@ -119,48 +119,4 @@ reRouter.get('/delete/:id', async (req, res) => {
     res.redirect('/dashboard');
 });
 
-reRouter.get('/update', async (req, res) => {
-    const recurringExpenses = await RecurringExpense.find();
-
-    for (const recurringExpense of recurringExpenses) {
-        const today = new Date();
-        const nextExpenseDate = new Date(recurringExpense.nextExpenceAt);
-
-        const isToday =
-            today.getFullYear() === nextExpenseDate.getFullYear() &&
-            today.getMonth() === nextExpenseDate.getMonth() &&
-            today.getDate() === nextExpenseDate.getDate();
-
-        if (isToday) {
-            const oneTimeExpense = new OneTimeExpense({
-                amount: recurringExpense.amount,
-                dateAndTime: recurringExpense.nextExpenceAt,
-                description: recurringExpense.description,
-                title: recurringExpense.title,
-                userid: recurringExpense.userid,
-            });
-            await oneTimeExpense.save();
-            await redis.set(
-                `${oneTimeExpense._id}-ote`,
-                JSON.stringify(oneTimeExpense),
-                'EX',
-                1800
-            );
-            recurringExpense.oneTimePayments.push(`${oneTimeExpense.id}`);
-        }
-
-        recurringExpense.lastExpenceAddedAt = recurringExpense.nextExpenceAt;
-        recurringExpense.nextExpenceAt = new Date(
-            getNextExpenseDate(
-                recurringExpense.nextExpenceAt,
-                recurringExpense.wayOfRepeats.repeats,
-                recurringExpense.wayOfRepeats.every
-            )
-        );
-
-        await recurringExpense.save();
-        await redis.set(`${recurringExpense._id}-re`, JSON.stringify(recurringExpense), 'EX', 1800);
-    }
-});
-
 module.exports = { reRouter };
