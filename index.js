d33d58de730ee661eaab8efcfc365aadd6a2bf1a
SMXTREME <132286622+SMXTREME@users.noreply.github.com>
2025-06-30 10:55:01 +0530

    added autoRemove to clear the databse after the session expires

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
-        store: MongoStore.create({ mongoUrl: MONGODB_URI }),
+        store: MongoStore.create({
+            mongoUrl: MONGODB_URI,
+            autoRemove: 'native',
+        }),
         secret: process.env.JWT_SECRET,
         resave: false,
         saveUninitialized: false,
